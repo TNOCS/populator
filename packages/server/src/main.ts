0309acf497eaa29c075e7ee5c4900c728bfb8c92
Floris <fc.vanwijk@gmail.com>
2021-03-04 13:11:05 +0100

    changed config

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -1,8 +1,19 @@
 import { NestFactory } from '@nestjs/core';
+import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
+import { Logger } from '@nestjs/common';
+
+const port = process.env.PORT || 3333;
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+    bodyParser: true,
+    logger: true,
+  });
+
+  await app.listen(port, () => {
+    Logger.log(`Populator rest service listening on port ${port}`);
+  });
 }
 bootstrap();
