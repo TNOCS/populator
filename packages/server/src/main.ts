0f7d8b58429af185629beb65646d917a8ab31a9b
Floris <fc.vanwijk@gmail.com>
2021-03-24 11:40:43 +0100

    added swagger

--- packages/server/src/main.ts
+++ packages/server/src/main.ts
@@ -2,6 +2,7 @@ import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app.module';
 import { Logger } from '@nestjs/common';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const port = process.env.PORT || 3333;
 
@@ -11,6 +12,15 @@ async function bootstrap() {
     bodyParser: true,
     logger: true,
   });
+
+  const config = new DocumentBuilder()
+    .setTitle('Population service')
+    .setDescription('The population service API endpoint documentation')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
 
   await app.listen(port, () => {
     Logger.log(`Populator rest service listening on port ${port}`);
