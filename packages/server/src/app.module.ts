96c07a35ac9fbec16214d0b4b4c8d8b7bc48d873
Floris <fc.vanwijk@gmail.com>
2021-03-24 11:33:55 +0100

    Moved database to app module

--- packages/server/src/app.module.ts
+++ packages/server/src/app.module.ts
@@ -1,16 +1,28 @@
 import { Module } from '@nestjs/common';
-import { ConfigModule } from '@nestjs/config';
-import { AnalyseController } from './analyse/analyse.controller';
-import { AnalyseModule } from './analyse/analyse.module';
-import { AnalyseService } from './analyse/analyse.service';
-import { DatabaseModule } from './database/database.module';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { AnalysisModule } from './analysis/analysis.module';
 
 @Module({
   imports: [
-    AnalyseModule,
-    DatabaseModule,
+    AnalysisModule,
     ConfigModule.forRoot({
       isGlobal: true,
+    }),
+    TypeOrmModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('POSTGRES_HOST'),
+        port: configService.get('POSTGRES_PORT'),
+        username: configService.get('POSTGRES_USER'),
+        password: configService.get('POSTGRES_PASSWORD'),
+        database: configService.get('POSTGRES_DB'),
+        entities: [__dirname + '/../**/*.entity.{js,ts}'],
+        logging: true,
+        verboseRetryLog: true,
+      }),
     }),
   ],
   controllers: [],
