a313d13d92d654d003bdaeb54adaa66368d2e51e
Floris <fc.vanwijk@gmail.com>
2021-06-28 10:28:59 +0200

    Added migration information back into summarised endpoint

--- packages/server/src/summarised/summarised.service.ts
+++ packages/server/src/summarised/summarised.service.ts
@@ -30,7 +30,7 @@ export class SummarisedService {
           'id', feature_rows.gid,
           'geometry',   ST_AsGeoJSON(st_transform(feature_rows.geovlak, 4326))::json, 
           'properties', row_to_json(feature_rows)::jsonb - 'geovlak'))) as featurecollection 
-          from (select gid, bu_naam, b.bu_code, wk_naam, gm_naam, postcode, aant_inw as t_res, aant_werkenden as t_non_res, t_00_14_jr, t_15_24_jr, t_25_44_jr, t_45_64_jr, t_65_eo_jr, aantal_hh, t_eenp_hh, t_hh_z_k, t_hh_m_k, gem_hh_gr, geovlak 
+          from (select gid, bu_naam, b.bu_code, wk_naam, gm_naam, postcode, aant_inw as t_res, aant_werkenden as t_non_res, t_00_14_jr, t_15_24_jr, t_25_44_jr, t_45_64_jr, t_65_eo_jr, aantal_hh, t_eenp_hh, t_hh_z_k, t_hh_m_k, gem_hh_gr, p_west_al, p_n_w_al, p_marokko, p_ant_aru, p_surinam, p_turkije, p_over_nw, geovlak 
                 from cbs.buurtdata b, 
                     (select sum(hh_size) as aant_werkenden, po.bu_code  
                     from bag.population po
@@ -41,7 +41,7 @@ export class SummarisedService {
           where st_contains(${geomQuery}, b.geovlak)
           and b.bu_code = werkenden.bu_code
           UNION ALL
-          select gid, bu_naam, bu_code, wk_naam, gm_naam, postcode, aant_inw as t_res, aant_werkenden as t_non_res, t_00_14_jr, t_15_24_jr, t_25_44_jr, t_45_64_jr, t_65_eo_jr, aantal_hh, t_eenp_hh, t_hh_z_k, t_hh_m_k, gem_hh_gr, geovlak 
+          select gid, bu_naam, bu_code, wk_naam, gm_naam, postcode, aant_inw as t_res, aant_werkenden as t_non_res, t_00_14_jr, t_15_24_jr, t_25_44_jr, t_45_64_jr, t_65_eo_jr, aantal_hh, t_eenp_hh, t_hh_z_k, t_hh_m_k, gem_hh_gr, p_west_al, p_n_w_al, p_marokko, p_ant_aru, p_surinam, p_turkije, p_over_nw, geovlak 
           from GetPartialBuurtData(${geomQuery})) feature_rows`,
       )
       .then((results: any) => {
